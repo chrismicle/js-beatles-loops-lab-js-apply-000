5b20bb8a283aaa71a193c3af1c8542db12c044ba
Christina Collura <cmcollura@gmail.com>
2017-11-26 22:59:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 function theBeatlesPlay(musicians, instruments) {
   var newArray = [];
   for (var i =0; i < musicians.length; i++) {
-    newArray.push(`#{musicians} plays #{instruments}`);
+    newArray.push(musicians[1] + 'plays' + instruments[1]);
   }
   return newArray;
 }
